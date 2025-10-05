aa480257bf51589e4e7d107d0896cab09bb5e5c3
innerweb404-web <innerweb404@gmail.com>
2025-10-05 22:25:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,14 +7,22 @@ newListItem.textContent = "Discord";
 socialList.appendChild(newListItem);
 socialList.style.cursor = 'pointer';
 socialList.addEventListener('click', (event) =>{
-    
-    if(event.target.tagName === 'LI'){
+    /*
+    Using closest instead of tagName is very necessary if there's
+    another element inside the <LI> element.
+    */
+    if(event.target.closest === 'LI'){
         socialList.querySelectorAll('li').forEach(li => {
         li.classList.remove('active');
         li.style.cursor = 'pointer';
         });
+        /*
+        Adding this event inside the if statement
+        would make it possible for me to access only the
+        list elements insie the <UL> tag.
+        */
+        event.target.classList.add('active');
     }
-    event.target.classList.add('active');
     
     
 });
